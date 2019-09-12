2df3bc5af1485116ffa013b4f9aaaab97952c407
Ayon Roy <royshouhag@gmail.com>
2019-09-13 02:53:35 +0600

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ const socket = io();
 
 const outputYou = document.querySelector('.output-you');
 const outputBot = document.querySelector('.output-bot');
+const btn = document.querySelector('button');
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -10,7 +11,7 @@ recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-document.querySelector('button').addEventListener('click', () => {
+btn.addEventListener('click', () => {
     recognition.start();
 });
 
@@ -41,6 +42,14 @@ recognition.addEventListener('error', (e) => {
 function synthVoice(text) {
     const synth = window.speechSynthesis;
     const utterance = new SpeechSynthesisUtterance();
+    const voices = window.speechSynthesis.getVoices();
+    utterance.volume = 1;
+    utterance.voice = window.speechSynthesis.getVoices().filter(function (voice) {
+        return voice.name == "Google UK English Female"
+
+    })[0];
+    utterance.rate = 1;
+    utterance.pitch = 1;
     utterance.text = text;
     synth.speak(utterance);
 }
