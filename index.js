e1eed28d0791a94f7e74fd980d7d7513eeb44dfc
Ayon Roy <royshouhag@gmail.com>
2019-09-13 18:28:43 +0600

    webhook

--- index.js
+++ index.js
@@ -2,9 +2,63 @@ const APIAI_TOKEN = "ca6ad02d83b24d6d81b920bba3180f4e" || process.env.DIALOGFLOW
 // const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
 
 const express = require('express');
-const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const apiai = require('apiai')(APIAI_TOKEN);
+const router = express.Router();
 
+
+const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// Handlebars Middleware
+// app.engine('handlebars', exphbs({
+//     defaultLayout: 'index'
+// }));
+// app.set('view engine', 'handlebars');
+// var handlebars = require('handlebars');
+
+// handlebars.registerHelper('for', function (from, to, incr, block) {
+//     var accum = '';
+//     for (var i = from; i < to; i += incr)
+//         accum += block.fn(i);
+//     return accum;
+// });
+// handlebars.registerHelper({
+//     eq: function (v1, v2) {
+//         return v1 === v2;
+//     },
+//     ne: function (v1, v2) {
+//         return v1 !== v2;
+//     },
+//     lt: function (v1, v2) {
+//         return v1 < v2;
+//     },
+//     gt: function (v1, v2) {
+//         return v1 > v2;
+//     },
+//     lte: function (v1, v2) {
+//         return v1 <= v2;
+//     },
+//     gte: function (v1, v2) {
+//         return v1 >= v2;
+//     },
+//     and: function () {
+//         return Array.prototype.slice.call(arguments).every(Boolean);
+//     },
+//     or: function () {
+//         return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+//     }
+// });
+
+
+// PUBLIC
 app.use(express.static(__dirname + '/views')); // html
 app.use(express.static(__dirname + '/public')); // js, css, images
 
@@ -50,4 +104,22 @@ io.on('connection', function (socket) {
 // Routers
 app.get('/', (req, res) => {
     res.sendFile('index.html');
+});
+
+app.get('/navigation', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/navigationSys.html'));
+});
+
+
+// Dialogflow Webhook
+app.post('/webhook', (req, res) => {
+
+    const link = req.body.parameters.Link;
+
+    res.sendFile(path.join(__dirname + '/views/navigationSys.html'));
+    // return res.json({
+    //     speech: dataToSend,
+    //     displayText: dataToSend,
+    //     source: 'get-movie-details'
+    // });
 });
