f40f8f5e3f8d352b4052bdccd148d48cdd1aa024
Ayon Roy <royshouhag@gmail.com>
2019-09-15 20:06:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -110,21 +110,13 @@ app.get('/navigation', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/navigationSys.html'));
 });
 
-// Dialogflow Webhook
-app.post('/webhook', (req, res) => {
+// // Dialogflow Webhook
+// app.post('/webhook', (req, res) => {
 
-    const link = req.body.parameters.Link;
-    alert(req.body);
-
-    // return res.json({
-    //     speech: 'done',
-    //     fulfillmentText: 'done',
-    //     source: 'MIST.AI'
-    // });
-});
-
-function nav() {
-    app.get('/navigation', (req, res) => {
-        res.sendFile(path.join(__dirname + '/views/navigationSys.html'));
-    });
-}
+//     const link = req.body.parameters.Link;
+//     // return res.json({
+//     //     speech: 'done',
+//     //     fulfillmentText: 'done',
+//     //     source: 'MIST.AI'
+//     // });
+// });
