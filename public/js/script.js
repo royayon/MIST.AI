7a8834ef1787bb175d96ef522a4704f8a1c196d4
Ayon Roy <royshouhag@gmail.com>
2020-02-25 22:41:52 +0600

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -57,7 +57,7 @@ recognition.addEventListener('result', (e) => {
 
 recognition.addEventListener('speechend', () => {
     recognition.stop();
-    // Button Style Change to GREENISH
+    // Button Style Change to GREENISHs
     btn.style.background = 'linear-gradient(180deg, #39C2C9 0%, #3FC8C9 80%, #3FC8C9 100%)';
 
     annyang.resume();
