a8615cd5d36c57ca4a841246ca22906c980ab46f
Ayon Roy <royshouhag@gmail.com>
2020-03-12 02:07:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -594,6 +594,9 @@ app.post('/webhook', (req, res) => {
         var ampm = hours >= 12 ? 'PM' : 'AM';
         hours = hours % 12;
         hours = hours ? hours : 12; // the hour '0' should be '12'
+        if (minutes === NaN) {
+            minutes = 0;
+        }
         minutes = minutes < 10 ? '0' + minutes : minutes;
         var strTime = hours + ':' + minutes + ' ' + ampm;
         return strTime;
