b48d7fa880717f215dc0fcc5f8ac3315257691c2
Ayon Roy <royshouhag@gmail.com>
2020-03-12 00:34:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -540,6 +540,41 @@ app.post('/webhookDialogflow', (req, res) => {
 
         addToDB(db.collection('reminders'), reminder);
 
+        let textResponse = `Setting up your reminder for ${title} for ${outputDate}`;
+        res.send(createTextResponse(textResponse));
+    }
+
+    function createTextResponse(textResponse) {
+        let response = {
+            "fulfillmentText": "This is a text response",
+            "fulfillmentMessages": [{
+                "text": {
+                    "text": [
+                        textResponse
+                    ]
+                }
+            }],
+            "source": "example.com",
+            "payload": {
+                "google": {
+                    "expectUserResponse": true,
+                    "richResponse": {
+                        "items": [{
+                            "simpleResponse": {
+                                "textToSpeech": "this is a simple response"
+                            }
+                        }]
+                    }
+                },
+                "facebook": {
+                    "text": "Hello, Facebook!"
+                },
+                "slack": {
+                    "text": "This is a text response for Slack."
+                }
+            }
+        }
+        return response;
     }
 
 });
